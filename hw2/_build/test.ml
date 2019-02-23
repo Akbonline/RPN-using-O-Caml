65e3a47792e628249ff38f0e3e1a03b256cd397b
Akshat Bajpai <35187768+Akbonline@users.noreply.github.com>
2019-02-22 23:47:05 -0500

    Update test.ml

--- hw2/_build/test.ml
+++ hw2/_build/test.ml
@@ -11,7 +11,6 @@ let test_fixture = "RPN" >:::
 		assert_equal ("Not enough arguments for *") (rpn.procRPN ["23";"*"]);
 		assert_equal (5.000000) (rpn.procRPN ["15"; "7"; "1"; "1"; "+"; "-"; "/"; "3"; "*"; "2"; "1"; "1"; "+"; "+"; "-"]);
 		assert_equal (3.0 **.2.0) (rpn.procRPN ["3";"2";"^"]);
-		assert_equal ("Incorrect RPN expression. Operator Overload") (rpn.procRPN ["aasdsf"]);
 	);
 ]
 
